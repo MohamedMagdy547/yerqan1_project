fab6493c78584fb7dec41aa180a4c4f7154134e9
MohamedMagdy547 <MohamedMagdi5004@gmail.com>
2024-06-18 21:48:55 +0200

    add some space to test

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,10 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const SpalshScreen(),
+      home: const SpalshScreen(
+
+
+      ),
     );
   }
 }
